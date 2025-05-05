5780346b37c56f5ae35a52a43abb99f32cc3f04b
ofek-n <ofeknachshoni@gmail.com>
2025-05-05 10:19:52 +0300

    deleted redundant files

--- funcctions/move_forward.m
+++
@@ -1,7 +0,0 @@
-function [refL, refR] = move_forward(posL, posR)
-% Move forward one cell
-
-cell_distance = 180; % mm or encoder units
-refL = posL + cell_distance;
-refR = posR + cell_distance;
-end
